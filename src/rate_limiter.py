08f669609a0e64ee0ef7f0e45f11f9a7d3375b04
maciejgz <macgzi@gmail.com>
2024-05-12 20:27:28 +0200

    defining global variable from .env

--- src/rate_limiter.py
+++ src/rate_limiter.py
@@ -2,11 +2,10 @@ from dotenv import load_dotenv
 from src.api import app
 import os
 
-
 load_dotenv()
+rate_limiter_algorithm = os.getenv("RATE_LIMITER_ALGORITHM")
 
 @app.middleware("http")
 async def add_process_time_header(request, call_next):
     response = await call_next(request)
-    print(os.getenv('RATE_LIMITER_ALGORITHM'))
     return response
